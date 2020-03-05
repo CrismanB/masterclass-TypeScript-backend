a0eb318ff324267e023928c2001e08106f51fc01
Crisman Bernardo Kima <crismanbernardokima@MacBook-Air-de-Crisman.local>
2020-03-05 05:55:28 -0300

    first commmit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import EmailService from "./../services/EmailService";
+const users = [{ name: "Crisman", email: "crisman@hotmail.com" }];
+
+export default {
+  async index(req: Request, res: Response) {
+    return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail({
+      to: {
+        name: "Crisman Santos",
+        email: "crisman@hotmail.com"
+      },
+      message: { subject: "bem vindo", body: "Welcome" }
+    });
+
+    return res.send();
+  }
+};
